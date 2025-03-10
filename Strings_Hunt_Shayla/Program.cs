e773f006bb29b60d4feb48c94fb4410cf387b8cc
Shayla <Shaylahunt834@gmail.com>
2025-03-10 14:31:27 -0500

    Update Program.cs

--- Strings_Hunt_Shayla/Program.cs
+++ Strings_Hunt_Shayla/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Asks user to input a character and assigns input to a variable.
+            Console.WriteLine("Please input a character!");
+            char myChar = Console.ReadKey().KeyChar;
+
+            bool isLetter = Char.IsLetter(myChar); // Checks if the character entered is a letter.
+
+            Console.WriteLine($"\n{myChar} is a letter: {isLetter}."); // Prints the results to console.
+
+            // Asks user a question and saves response to a variable.
+            Console.WriteLine("Why do you like video games?");
+            string sentence = Console.ReadLine();
+
+            // Asks user to enter a word, saves response to a variable called word.
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+
+            bool isIn = sentence.Contains(word); // Checks if the word is in the sentence.
+
+            Console.WriteLine($"{word} exists in {sentence}, {isIn}!");// Prints the results to console.
+
         }
     }
 }
